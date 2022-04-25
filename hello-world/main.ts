088f64a68811cddf4b4957f3c03f55db6e724585
Mihai Ionut Vilcu <ionutvmi@gmail.com>
2022-04-25 12:32:04 +0300

    Added support for npm workspaces.

--- hello-world/main.ts
+++ hello-world/main.ts
@@ -1,8 +1,13 @@
 import { add } from "./math";
 
+// this is a local package which is nice
+import { multiply } from "my-advanced-lib";
+
 console.log("Hello from a typesafe world", add(11, 15));
 
 // checking the debugger
 const result = add(add(4, 70), 15);
 
 console.log(`The computed result is ${result}`);
+
+console.log(`This is multiplication from a local package ${multiply(4, 7)}`);
